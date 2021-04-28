60d5d0674d8626cd0e6219a0fc6e1cbf59ac6f62
justiyz <inyange@yahoo.com>
2021-04-28 18:53:15 +0100

    controller test

--- src/test/java/com/vehicleapp/web/controller/VehicleControllerTest.java
+++ src/test/java/com/vehicleapp/web/controller/VehicleControllerTest.java
@@ -14,8 +14,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -74,6 +73,22 @@ class VehicleControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
+    }
+
+    @Test
+    void testTheUpdateVehicleEndpoint() throws Exception {
+         Vehicle vehicle = new Vehicle();
+         vehicle.setId(10L);
+         vehicle.setVehicleName("New Vehicle Name");
+
+        this.mockMvc.perform(put("/vehicle/update")
+                .contentType("application/json")
+                .content(mapper.writeValueAsString(vehicle)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+
     }
 
 
